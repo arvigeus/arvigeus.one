da9dd8ec6611fa36cba71e82bc9a9676e886de3f
Nikolay Stoynov <arvigeus@gmail.com>
2021-11-08 21:15:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,134 @@
+# Home server
+
+## System information
+
+- Debian 11
+
+## Setup
+
+Rename `.env.example` to `.env`
+
+Download caddy executable from [here](https://caddyserver.com/download) and move it in `config/caddy/caddy`
+
+### Docker
+
+It will be installed and configured as part of `./run.sh setup`
+
+### User
+
+Uncomment the following line in /etc/pam.d/su, by removing the leading '#':
+
+```text
+auth required pam_wheel.so
+```
+
+Create the group wheel with root privileges:
+
+```sh
+addgroup --system wheel
+```
+
+Execute `visudo` and add the following line:
+
+```text
+%wheel  ALL=(ALL)       NOPASSWD: ALL
+```
+
+Create user and add it to the `wheel` group:
+
+```sh
+useradd -m -g wheel arvigeus
+passwd arvigeus
+```
+
+#### User misc
+
+Use `bash` instead of `sh`
+
+```sh
+sudo chsh -s /bin/bash $(whoami)
+```
+
+Get user info
+
+```sh
+id
+```
+
+Get all groups:
+
+```sh
+less /etc/group
+```
+
+## Running containers
+
+Allow `run.sh` to be executed:
+
+```sh
+sudo chmod +x run.sh
+```
+
+### Server
+
+**Make sure volumes for `filestash` are commented out, also the media volumes for `kavita` and `dim`!**
+
+```sh
+# First time run
+./run.sh setup
+
+# manage containers:
+./run.sh start
+./run.sh stop
+./run.sh restart
+
+# A manual hack for extra tweaks
+./run.sh post-setup
+```
+
+## Databases
+
+### Add new database to postgresql
+
+```sh
+docker exec -it postgres psql
+```
+
+```sql
+CREATE USER $database WITH PASSWORD '$database';
+CREATE DATABASE $database;
+GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+```
+
+Replace `$database` with database name
+
+### Remove database from postgresql
+
+```sh
+docker exec -it postgres psql
+```
+
+```sql
+DROP USER $database;
+DROP DATABASE $database;
+```
+
+Replace `$database` with database name
+
+## Tips and tricks
+
+### Prettify Nextcloud links
+
+Add `'htaccess.RewriteBase' => '/',` to `${DATA}/nextcloud/config/config.php`
+
+Reload htaccess rules: `docker exec -u www-data nextcloud php occ maintenance:update:htaccess`
+
+## Troubleshooting
+
+- `sudo: unable to resolve host localhost.localdomain: Name or service not known`: Add the following line to `/etc/hosts/`: `127.0.0.1 localhost.localdomain localhost`
+- Check if website is reachable: `nslookup arvigeus.one` and `nslookup arvigeus.one 8.8.8.8` should return the same
+- Check logs: `sudo docker container logs caddy`
+- Some containers take a lot of time to boot, meanwhile they can return 404 (not found) or 502 (bad gateway) errors
+- Check why site is not secure: <https://www.whynopadlock.com>
+- Check if docker is working: `curl -H "Content-Type: application/json" --unix-socket /var/run/docker.sock http://localhost/_ping`
+- Uncomment `DOZZLE_LEVEL: debug` in `docker-compose.yml`, then restart to see more detailed log
---
+++ run.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+ENV=.env # Do not touch
+
+function setup {
+    export $(cat $ENV | sed 's/#.*//g' | xargs)
+
+    sudo apt update
+
+    # Docker
+    sudo apt-get install apt-transport-https software-properties-common ca-certificates curl gnupg lsb-release -y
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt -y install docker-ce docker-ce-cli
+    curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url | grep docker-compose-Linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+    chmod +x docker-compose-Linux-x86_64
+    sudo mv docker-compose-Linux-x86_64 /usr/bin/docker-compose
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo docker network create caddy_net
+    sudo usermod -a -G docker $USER
+
+    # Cockpit
+    sudo wget -qO - https://repo.45drives.com/key/gpg.asc | sudo gpg --dearmor -o /usr/share/keyrings/45drives-archive-keyring.gpg
+    sudo curl -sSL https://repo.45drives.com/lists/45drives.sources -o /etc/apt/sources.list.d/45drives.sources
+    sudo apt update
+    sudo apt install -y cockpit cockpit-navigator
+    sudo cp $CONFIG/cockpit/cockpit.conf /etc/cockpit/cockpit.conf
+    
+    # UFW
+    sudo apt install -y ufw
+    sudo ufw allow 443/tcp comment "caddy"
+    sudo ufw allow 80/tcp comment "caddy"
+    sudo ufw allow 22/tcp comment 'Open port ssh tcp port 22'
+    sudo ufw allow 9090/tcp comment 'Cockpit'
+    sudo ufw enable
+
+    # Postgres
+    sudo chmod +x $CONFIG/postgres/docker-entrypoint-initdb.d/create-multiple-postgresql-databases.sh
+}
+
+function cleanup {
+    sudo docker image prune -a
+}
+
+function start {
+    echo Starting services $*
+    if [ -z "$1" ]; then
+        sudo docker-compose --env-file $ENV up -d --build
+    else
+        for ARG in $*
+        do
+            sudo docker-compose --env-file up -d --build $ENV $ARG
+        done
+    fi
+    sudo docker-compose exec nextcloud chown -R 82:root /var/www/html
+    docker container logs caddy
+}
+
+function stop {
+    echo Stopping services $*
+    if [ -z "$1" ]; then
+        sudo docker-compose --env-file $ENV down --remove-orphan
+    else
+        for ARG in $*
+        do
+            sudo docker-compose --env-file $ENV rm -s -v $ARG
+        done
+    fi
+}
+
+function restart {
+    stop $*
+    start $*
+}
+
+function post-setup {
+    docker exec -u www-data app-server php occ --no-warnings app:install calendar
+    docker exec -u www-data app-server php occ --no-warnings app:install contacts
+    docker exec -u www-data app-server php occ --no-warnings app:install notes
+    docker exec -u www-data app-server php occ --no-warnings app:install music
+    docker exec -u www-data app-server php occ --no-warnings app:install breezedark
+
+    docker cp filestash:/app/data/state $DATA/filestash
+    sudo chmod -R 777 $DATA/filestash
+
+    stop $*
+
+    echo "Now uncomment volumes for `filestash`, `kavita`, `dim`, then run `./run.sh start`"
+}
+
+function default {
+    # Default task to execute
+    help
+}
+
+function help {
+    echo "$0 <task> <args>"
+    echo "Tasks:"
+    compgen -A function | cat -n
+}
+
+TIMEFORMAT="Task completed in %3lR"
+time ${@:-default}
