a8db38f445d90fcf1a892189b5e3be648c674c5f
Nikolay Stoynov <arvigeus@gmail.com>
2026-06-23 13:34:09 +0700

    fix devbox again

--- services/devbox/start.sh
+++ services/devbox/start.sh
@@ -9,9 +9,9 @@ source <(grep -v '^#' "../../.env" | grep -v '^$')
 set +a
 
 : "${DATA:?DATA is not set in .env}"
-: "${OPENCODE_WORKSPACE:?OPENCODE_WORKSPACE is not set in .env}"
+: "${HOST_WORKSPACE:?HOST_WORKSPACE is not set in .env}"
 : "${PUID:?PUID is not set in .env}"
 : "${PGID:?PGID is not set in .env}"
 
-sudo mkdir -p "$DATA/devbox/home" "$OPENCODE_WORKSPACE"
-sudo chown -R "$PUID:$PGID" "$DATA/devbox" "$OPENCODE_WORKSPACE"
+sudo mkdir -p "$DATA/devbox/home" "$HOST_WORKSPACE"
+sudo chown -R "$PUID:$PGID" "$DATA/devbox" "$HOST_WORKSPACE"
